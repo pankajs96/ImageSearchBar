3f07e4919ac1ec663e52b5eb9753448134268a6a
pankaj <pankaj98052sharma@gmail.com>
2021-08-15 00:49:36 +0530

    Final

--- src/components/App.js
+++ src/components/App.js
@@ -20,6 +20,7 @@ onSearchSubmit= async (term)=>{
     render(){
         return(
             <div className = "ui container" style={{ marginTop: '10px' }}>
+                <h1>Search AnyThing You Want To</h1>
                 <SearchBar onSubmit={this.onSearchSubmit} />
                <ImageList images={this.state.images} />
             </div>
